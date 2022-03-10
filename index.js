4f6e9a1237255c8686192b3f0f49508854979726
Daniel <dannyb829@gmail.com>
2022-03-10 13:09:03 -0500

    commit tree f8110ff59d2a59b2642d38e35ee8aa205deb3ba3

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split("").reverse().join("")
 }
 
 /* 
   Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  the function takes in a 'word' as an arguement
+  if (word same as word backwards)
+  return true
+  else 
+  return false
+  */
+ 
+ /*
+ Add written explanation of your solution here
+ I need to create a function that checks if a word reads the same backwards as forwards
+ to reverse word turn word into array using split, then apply reverse, then join reversed array back into a string
+ compare this to the original word
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,11 +24,23 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
+  
   console.log("");
-
+  
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("rotor"))
+  
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("apple"));
+  
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("people"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
 }
 
 module.exports = isPalindrome;
